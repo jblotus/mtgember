fb5e2a79b277d42c279a2eca2fac10c2ae07ca0a
James Fuller <jblotus@gmail.com>
2012-05-02 23:27:11 -0400

    added some stuff

--- js/app.js
+++ js/app.js
@@ -4,20 +4,37 @@ App.Card = Em.Object.extend({
   name: '',
   tapped: false
 });
+
+App.Cards = Em.Object.create({
+  cards:  [],
+  shuffle : function() { 
+    var o = this.get('cards');
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    this.set('cards', o);
+  }
+});
  
 
 App.PlayMatView = Em.View.extend({
-  id: 'playmat',
-  contentBinding: 'App.cardsController.content'
+  id: 'playmat'
 });
 
-App.cardsController = Ember.Object.create({
-  content: Ember.Object.create({
-    cards : [
-     App.Card.create({ name: 'Grizzly Bears', img_src: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129586&type=card', type: 'creature',  power: 2, toughness: 2, tapped: true }),
-     App.Card.create({ name: 'Gravecrawler', img_src: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=222902&type=card', type: 'creature',  power: 2, toughness: 1 })
-    ]
-  })
+App.cardsController = Ember.ArrayController.create({
+
+  contentBinding: 'App.Cards.cards',
+
+  refresh: function() {
+    App.Cards.set('cards', [
+      App.Card.create({ name: 'Grizzly Bears', img_src: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129586&type=card', type: 'creature',  power: 2, toughness: 2, tapped: true }),
+      App.Card.create({ name: 'Gravecrawler', img_src: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=222902&type=card', type: 'creature',  power: 2, toughness: 1 })
+    ]);
+
+    return this;
+  },
+
+  shuffle: function() {
+    App.Cards.shuffle();
+  }
 });
 
 Handlebars.registerHelper('ifCreature', function(type, options) {
@@ -47,3 +64,5 @@ App.CardView = Em.View.extend({
     this.toggleTapped();
   }
 });
+
+App.cardsController.refresh().shuffle();
